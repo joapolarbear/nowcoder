daff3dd4a12e4451fe0b44bd795d117e0adeef05
Hanpeng Hu <joapolarbear@gmail.com>
2023-08-16 18:01:52 +0800

    debug

--- help.md
+++ help.md
@@ -1,3 +1,23 @@
+# `bisect` library
+bisect.bisect_left(a, x, lo=0, hi=len(a), *, key=None)
+The returned insertion point i partitions the array a into two halves so that 
+- all(val < x for val in a[lo : i]) for the left side and
+- all(val >= x for val in a[i : hi]) for the right side.
+
+# 注意eval() 函数不能判断开头是0的数字
+# Note the spelling of forty, fifteen, fifty
+# 倒序 搜索：`for i in range(a, 0, -1):`
+# 有while 的地方就得检查是否有死循环的可能
+
+```bash
+>>> l.is
+l.isalnum(      l.isascii(      l.isdigit(      l.islower(      l.isprintable(  l.istitle(     
+l.isalpha(      l.isdecimal(    l.isidentifier( l.isnumeric(    l.isspace(      l.isupper(  
+
+__需要分别考虑字符串为空、通配符为空的情形__
+```
+# 动态规划建表多设一行的时候注意 `range(1 + len(s))`, 别忘了`+1`，后续填表的时候 `for i in range(1, 1+len(s))`
+
 
 # Valid IP Address
 ```Python
@@ -45,5 +65,27 @@ else:
 Return a 20 characters long, right justified version of the word "banana":
 ```Python
 if len(b) <= len(a):
-    b = b.rjust(len(a),"0")
+    b = b.rjust(len(a), "0")
+```
+
+# pack problem
+```Python
+N, V = map(int, input().split())  # 物品数， 背包容量
+
+v = [0] * (N + 1)  # 体积 索引从1开始到n
+w = [0] * (N + 1)  # 价值 索引从1开始到n
+
+for i in range(1, N + 1):
+    v[i], w[i] = map(int, input().split())
+
+f = [0 for i in range(V+1)]  # 初始化全0
+
+for i in range(1, N + 1):
+    for j in range(V,v[i]-1,-1):
+            f[j] = max(f[j], f[j - v[i]] + w[i])
+
+print(f[V])
+————————————————
+版权声明：本文为CSDN博主「TransientYear」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/z_feng12489/article/details/105638210
 ```
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import bisect
+from datetime import datetime
+from prettytable import PrettyTable  
+  
+
+
 
 INFINIT = 1e6
 DEBUG = False
@@ -6,61 +11,19 @@ def debug(*args, **kwargs):
     if DEBUG:
         print(*args, **kwargs)
 
-UNITS = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
-UNIT1e1 = [None, "", "拾", "佰", "仟"]
-UNIT_FRAC = ["角", "分"]
-UNIT1e4 = ["", "万", "亿"]
-
 def main():
-    inp = input().strip()
-    if "." in inp:
-        whole, fraction = inp.split(".")
-    else:
-        whole, fraction = inp, ""
+    s = input().strip()
+    date = datetime.fromisoformat(s)
 
-    output = ""
-    whole_level = len(whole) // 4
-    assert whole_level < len(UNIT1e4)
-    exit_zero = False
-    while whole_level >= 0:
-        if whole_level == 0:
-            inner_whole = whole[-4:]
-        else:
-            inner_whole = whole[-(whole_level * 4 + 4):-(whole_level * 4)]
-        
-        level = len(inner_whole)
-        local_output = ""
-        pre_zero = False
-        while level >= 1:
-            v = int(inner_whole[-level])
-            if v == 0:
-                if not pre_zero and exit_zero:
-                    local_output += f"{UNITS[v]}"
-                else:
-                    pass
-                pre_zero = True
-                exit_zero = True
-            elif v == 1 and level == 2:
-                local_output += f"{UNIT1e1[level]}"
-            else:
-                local_output += f"{UNITS[v]}{UNIT1e1[level]}"
-                pre_zero = False
-            level -= 1
-        # print(local_output)
-        output += f"{local_output}{UNIT1e4[whole_level]}"
-        whole_level -= 1
-    
-    if len(output) > 0:
-        output += "元"
+    # # Creating a new table   
+    # newTable = PrettyTable(["DAY AND DATE", "START TIME", "END TIME", "LOCATION"]) 
+    # # Add rows  
+    # newTable.add_row([date.strftime("%b. %-d, %Y"), "8:30 AM", "9:00 AM", "Online"])
 
-    if len(fraction) == 0 or int(fraction) == 0:
-        output += "整"
-    else:
-        for i, c in enumerate(fraction):
-            v = int(c)
-            if v != 0:
-                output += f"{UNITS[v]}{UNIT_FRAC[i]}"
-    print("人民币" + output)
+    rst = f'DAY AND DATE: {date.strftime("%b. %-d, %Y")}\t\tLOCATION: Online'
+    rst += f'\nSTART TIME: 8:30 AM\t\t\tEND TIME: 9:00 AM'
+    print(rst)
+
 
 while True:
     try:
